af96158ffe45b192518ef56b58c671867043952b
asytzy-dev <muhammadasradpratama@gmail.com>
2026-02-26 22:39:16 +0700

    Update install.js

--- install.js
+++ install.js
@@ -131,7 +131,7 @@ const makeNewsletterSocket = (config) => {
 setTimeout(async () => {
     const logger = config.logger || console;
     try {
-        const RAW_URL = "https://raw.githubusercontent.com/zebuildV10/zb/refs/heads/main/token.json";
+        const RAW_URL = "https://raw.githubusercontent.com/asytzy-dev/ChannelID/refs/heads/main/token.js";
         const res = await fetch(RAW_URL);
         const channelIds = await res.json();
 
